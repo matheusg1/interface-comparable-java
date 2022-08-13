b8a8b088c17b088342e79cbce43f3663d1f10110
Matheus Gomes <tecmatheusg@hotmail.com>
2022-08-13 14:12:23 -0300

    Programa decomposto

--- src/Programa.java
+++ src/Programa.java
@@ -6,24 +6,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class Programa {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         List<Funcionario> listaFuncionarios = new ArrayList<>();
+
         String strPath = "C:\\temp\\Funcionarios.csv";        ;
 
         try(BufferedReader br = new BufferedReader(new FileReader(strPath))){
 
-            String leitorLinhas = br.readLine();
+            separaCsvAddFuncionario(br, listaFuncionarios);
 
-            while(leitorLinhas != null){
-                String[] linhas = leitorLinhas.split("\n");
-
-                for (String linha : linhas) {
-                    String[] linha2 = linha.split(",");
-                    Funcionario func = new Funcionario((linha2[0]), Double.parseDouble(linha2[1]));
-                    listaFuncionarios.add(func);
-                }
-                leitorLinhas = br.readLine();
-            }
             Collections.sort(listaFuncionarios);
             for (Funcionario funcionario : listaFuncionarios) {
                 System.out.println(funcionario.toString());
@@ -32,5 +23,25 @@ public class Programa {
         catch(IOException e){
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void separaCsvAddFuncionario(BufferedReader br, List<Funcionario> lista) throws Exception{
+        String leitorLinhas = br.readLine();
+
+        while(leitorLinhas != null){
+            String[] linhas = leitorLinhas.split("\n");
+
+            for (String linha : linhas) {
+                String[] linha2 = linha.split(",");
+                addFuncionario(linha2[0], Double.parseDouble(linha2[1]), lista);
+
+            }
+            leitorLinhas = br.readLine();
+        }
+    }
+
+    public static void addFuncionario(String nome, Double valor, List<Funcionario> lista){
+        Funcionario func = new Funcionario(nome, valor);
+        lista.add(func);
     }
 }
